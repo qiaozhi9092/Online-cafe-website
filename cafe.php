aabaafb20c1db01563f4ec2d29329b8a60a6ff08
qiaozhi9092 <qiaozhi9092@gmail.com>
2016-02-04 23:21:59 -0800

    add documents

---
+++ cafe.php
@@ -0,0 +1,171 @@
+<?php
+/*
+ * Template Name: My Custom Page
+ * Description: A Page Template with a darker design.
+ */
+
+// Code to display Page goes here...?>
+<?xml version="1.0" encoding="UTF-8"?>
+  <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+      "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+  <head>
+      <title>My Business</title>
+      
+    <meta name="authors" content= "Zhi Qiao" />
+	<meta name="description" content= "Business of Zhi Qiao" />
+	<meta name="keywords" content= "ucla, business, PIC40a" />
+	<meta name="copyright" content= "Copyright 2014 Zhi Qiao, All Rights Reserved" />
+
+<link href="stylecafe.css" rel="stylesheet" />
+  
+  </head>
+  <body>
+<div id="main">
+<div id="header">
+<h1 id="BusinessName">Zhi’s&reg; cafe</h1>
+<blockquote id="Quote"> <p> "Come in and enjoy!" </p> </blockquote>
+</div>
+
+
+<div id="dropdown">
+
+<ul class="d">
+		<li><a href="cafe.html">Home</a></li>
+		<li><a href="#">About Us</a>
+			<ul class="c">
+				<li><a href="#table">Some Products</a></li>
+				<li><a href="#content">How I Get Started</a></li>
+
+			</ul>
+		</li>
+		<li><a href="#">Extra</a>
+
+			<ul class="c">
+				<li><a href="#popular">Most Popular Items</a></li>
+				<li><a href="#companies">Some Other Cafes</a></li>
+				<li><a href="#footer">Contact Us</a></li>
+
+
+			</ul>
+		</li>
+	    <li><a href="cafeorder.html">Order Here</a></li>
+		
+</ul>	
+</div>
+
+
+
+
+
+
+<div id="sidebar">
+    <h2 id="popular">&sect;Most Popular Products</h2>
+ <ol>
+  <li>Switzerland hot chocolate &trade;</li>
+  <li>Chicken sandwich &trade;</li>
+  <li>Soup of the day&trade;</li>
+  <li>Veggie burger&trade;</li>
+ </ol>
+ 
+ <h2 id="companies">&sect;Cafe Websites</h2>
+ <ul class="c">
+  <li><a href="http://cafegratitudela.com//">Gratitude&reg;</a></li>
+  <li><a href="http://http://cafevida.net/cafevida/">Vida&reg;</a></li>
+  <li><a href="http://www.mcafedechaya.com/">M&reg;</a></li>
+ </ul>
+</div>
+
+
+
+
+
+
+
+
+
+<div id="content">	
+<h2 id="aboutus">&sect;About Us</h2>
+<p >
+<img src="Zhi.jpg" alt="Zhi" id="Zhi"  />
+	Greeting. Zhi Qiao, president and founder of Zhi's cafe. 
+I transfered from Foothill college in Bay Area in 2012 fall. 
+After came to UCLA I started my own cafe business 
+and it was the best decision of my life!
+    </p>
+    <p>
+    We have the most tasty chicken sandwichs in town! 
+    plus an authentic Switzerland hot chocolate will make your day!
+   
+   </p>
+   
+   <table id="table" border="1">
+  <caption>Some of our products</caption>
+  <tr>
+   <th>Product</th>
+   <th>Description</th>
+   <th>Price</th>
+  </tr>
+  <tr>
+   <td>Chicken sandwich &trade;</td>
+   <td>Chicken with avocado</td>
+   <td>6$ 99&cent;</td>
+  </tr>
+  <tr>
+   <td>Beef wraps&trade;</td>
+   <td>Comes with fries</td>
+   <td>5$ 99&cent;</td> 
+  </tr>
+  <tr>
+   <td>Soup of the day&trade;</td>
+   <td>Onion/tomato/mushroom </td>
+   <td>4$ 99&cent;</td>
+  </tr>
+  <tr>
+   <td>Veggie burger&trade;</td>
+   <td>Comes with fries</td>
+   <td>6$ 99&cent;</td>
+  </tr>
+  <tr>
+   <td>Switzerland hot chocolate &trade;</td>
+   <td>Comes with a french toast</td>
+   <td>3$ 99&cent;</td>
+  </tr>
+ </table>
+</div>	
+  
+<div id="footer">
+  <h2 id="Contact">&sect;Contact us at</h2>
+  
+   <address>
+  Zhi's Cafe &reg;<br/>
+  12345 street, <br/>
+  Fake city, <br/>
+  California 12345<br/>
+  USA<br/>
+ </address>
+	
+ <p>
+  <a href="http://validator.w3.org/check?uri=referer"><img
+           src="http://www.w3.org/Icons/valid-xhtml11"
+           alt="Valid XHTML 1.1" height="31" width="88" /></a>
+    <a href="http://jigsaw.w3.org/css-validator/">
+       <img style="border:0;width:88px;height:31px"
+       src="http://jigsaw.w3.org/css-validator/images/vcss" 
+       alt="Valid CSS!" />
+ </a>
+
+  </p>
+  <p class="b"> &copy; 2013 Zhi's &reg; Cafe 
+  </p>
+</div>
+  
+
+  
+  
+  </div>
+  </body>
+</html>
